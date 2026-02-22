3dd792bf4a8f6b8b6cfca372b07a578241073f0e
carstereos <carstereos@gmail.com>
2026-02-21 18:49:38 -0600

    Update values.py

--- opendbc/car/nissan/values.py
+++ opendbc/car/nissan/values.py
@@ -55,11 +55,6 @@ class NissanPlatformConfig(PlatformConfig):
 
 
 class CAR(Platforms):
-  # Rogue 2020
-  NISSAN_ROGUE = NissanPlatformConfig(
-    NissanCarDocs("Nissan Rogue 2020")],
-    NissanCarSpecs(mass=1610, wheelbase=2.705)
-  )
   NISSAN_XTRAIL = NissanPlatformConfig(
     [NissanCarDocs("Nissan X-Trail 2017")],
     NissanCarSpecs(mass=1610, wheelbase=2.705)
